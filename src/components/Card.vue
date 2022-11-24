a7bf4c79a9f763a3e927ff76f43ba7bbab2fd105
ahmadRamadhan-dotcom <ahmadoneperson@gmail.com>
2022-11-24 22:35:51 +0700

    chore: responsive components wrapper and card for example components

---
+++ src/components/Card.vue
@@ -0,0 +1,30 @@
+<template>
+  <div v-for="(data, index) in tailwindExample" id="example_card" class="">
+    <a href="#">
+      <img
+        :src="data.image"
+        id="thumbnail"
+        alt="thumbnail"
+        class="p-[.25rem] border-2"
+      />
+      <p
+        id="title_project_example"
+        class="hover:text-[#0056b3] mt-3 hover:underline text-[#007bff] font-medium text-xl font-['Arial']"
+      >
+        {{ data.projectName }}
+      </p>
+      <p
+        id="desc_project_example"
+        class="font-['Arial'] text-[#6c757d] font-normal text-base mt-3"
+      >
+        {{ data.descProject }}
+      </p>
+    </a>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  tailwindExample: Array,
+});
+</script>
--- src/views/Homeview.vue
+++ src/views/Homeview.vue
@@ -1,8 +1,65 @@
 <template>
   <Header />
-  <main></main>
+  <main>
+    <section class="p-12 md:p-12">
+      <div class="mt-14 md:mt-8">
+        <p
+          id="title_example"
+          class="font-['Arial'] leading-[35px] font-medium text-[#212529] text-[32px]"
+        >
+          Custom components
+        </p>
+        <p
+          id="desc_example"
+          class="font-medium mt-4 text-base text-[#212529] font-['Arial'] lg:w-[81%]"
+        >
+          Brand new components and templates to help folks quickly get started
+          with Bootstrap and demonstrate best practices for adding onto the
+          framework.
+        </p>
+      </div>
+      <div
+        id="example_card_container"
+        class="mt-6 grid grid-cols-1 gap-8 sm:grid-cols-2 sm:gap-6 md:grid-cols-3 xl:grid-cols-4 xl:gap-8"
+      >
+        <Card :tailwind-example="fakeComponentsData" />
+      </div>
+    </section>
+  </main>
 </template>
 
 <script setup>
 import Header from "~/components/Header.vue";
+import Card from "~/components/Card.vue";
+
+const fakeComponentsData = [
+  {
+    id: 1,
+    image: "https://getbootstrap.com/docs/4.2/assets/img/examples/album.png",
+    projectName: "Album",
+    descProject:
+      "Simple one-page template for photo galleries, portfolios, and more.",
+  },
+  {
+    id: 2,
+    image: "https://getbootstrap.com/docs/4.2/assets/img/examples/pricing.png",
+    projectName: "Pricing",
+    descProject:
+      "Example pricing page built with Cards and featuring a custom header and footer.",
+  },
+  {
+    id: 3,
+    image: "https://getbootstrap.com/docs/4.2/assets/img/examples/checkout.png",
+    projectName: "Checkout",
+    descProject:
+      "Custom checkout form showing our form components and their validation features.",
+  },
+  {
+    id: 4,
+    image: "https://getbootstrap.com/docs/4.2/assets/img/examples/product.png",
+    projectName: "Product",
+    descProject:
+      "Lean product-focused marketing page with extensive grid and image work.",
+  },
+];
 </script>
