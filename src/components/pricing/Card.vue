95fac6bf8ac9884cd051bab8d4f09f97336b412a
ahmadRamadhan-dotcom <ahmadoneperson@gmail.com>
2022-11-25 12:55:30 +0700

    chore: responsive pricing for all devices

---
+++ src/components/pricing/Card.vue
@@ -0,0 +1,68 @@
+<template>
+  <article
+    v-for="(data, index) in pricingData"
+    :key="index"
+    id="card_container"
+    :class="index == 2 ? 'sm:col-span-2 md:col-span-1' : ''"
+    class="shadow-sm rounded-sm h-[20.8em]"
+  >
+    <div
+      id="plan"
+      class="h-[3.5em] grid place-content-center place-items-center"
+    >
+      <span
+        class="font-normal text-[#212529] text-[21px] md:text-2xl text-center"
+        >{{ data.plan }}</span
+      >
+    </div>
+    <div id="card_content" class="mt-5">
+      <p
+        id="price"
+        class="text-center font-medium text-[#212529] text-[35px] md:text-[40px]"
+      >
+        ${{ data.price }}
+        <span class="font-normal text-[#6c757d] text-[32px]"> / mo</span>
+      </p>
+      <p
+        v-for="(item, index) in data.details"
+        id="detail"
+        :key="index"
+        :class="index == 0 ? 'mt-5' : ''"
+        class="text-[#212529] text-base text-center font-normal"
+      >
+        {{ item.item }}
+      </p>
+    </div>
+    <div class="mt-4 flex justify-center">
+      <button
+        :class="
+          data.price != 0
+            ? 'bg-[#007bff] text-white'
+            : 'text-[#007bff] hover:text-white'
+        "
+        class="text-xl hover:bg-[#007bff] transition duration-400 font-normal w-10/12 py-1.5 rounded-sm text-center border border-[#007bff]"
+        type="button"
+        id="button_action"
+      >
+        {{ data.action }}
+      </button>
+    </div>
+  </article>
+</template>
+
+<script setup>
+const props = defineProps({
+  pricingData: Array,
+});
+</script>
+
+<style scoped>
+#plan {
+  background: rgba(0, 0, 0, 0.03);
+  border-bottom: 1px solid rgba(0, 0, 0, 0.125);
+}
+
+#card_container {
+  border: 1px solid rgba(0, 0, 0, 0.125);
+}
+</style>
--- src/views/Pricingview.vue
+++ src/views/Pricingview.vue
@@ -1,3 +1,102 @@
 <template>
-  <main></main>
+  <main
+    class="grid place-content-center place-item-center w-11/12 xl:w-[1110px] mx-auto mt-3 md:mt-10"
+  >
+    <div
+      class="text-center grid place-content-center place-item-center xl:w-[700px] mx-auto"
+    >
+      <span
+        class="text-[49px] lg:text-[56px] font-[300] font-['Arial'] text-[#212529]"
+        >Pricing
+      </span>
+      <p class="font-['300'] text-xl text-[#212529]">
+        Quickly build an effective pricing table for your potential customers
+        with this Bootstrap example. It’s built with default Bootstrap
+        components and utilities with little customization.
+      </p>
+    </div>
+    <div
+      class="grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3 xl:gap-8 mt-12 xl:w-[940px]"
+    >
+      <Card :pricing-data="pricingData" />
+    </div>
+  </main>
 </template>
+
+<script setup>
+import Card from "~/components/pricing/Card.vue";
+const pricingData = [
+  {
+    id: 1,
+    plan: "Free",
+    price: 0,
+    details: [
+      {
+        id: 1,
+        item: "10 users included",
+      },
+      {
+        id: 2,
+        item: "2 GB of storage",
+      },
+      {
+        id: 3,
+        item: "Email support",
+      },
+      {
+        id: 4,
+        item: "Help center access",
+      },
+    ],
+    action: "Sign up for free",
+  },
+  {
+    id: 2,
+    plan: "Pro",
+    price: 15,
+    details: [
+      {
+        id: 1,
+        item: "20 users included",
+      },
+      {
+        id: 2,
+        item: "10 GB of storage",
+      },
+      {
+        id: 3,
+        item: "Priority email support",
+      },
+      {
+        id: 4,
+        item: "Help center access",
+      },
+    ],
+    action: "Get started",
+  },
+  {
+    id: 3,
+    plan: "Enterprise",
+    price: 29,
+    details: [
+      {
+        id: 1,
+        item: "30 users included",
+      },
+      {
+        id: 2,
+        item: "15 GB of storage",
+      },
+      {
+        id: 3,
+        item: "Phone and email support",
+      },
+      {
+        id: 4,
+        item: "Help center access",
+      },
+    ],
+    action: "Contact us",
+  },
+];
+</script>
